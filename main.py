f8050b5a84b6297bbe74469f0a645ad4fa2cbc47
ReikaRolfini <121880927+ReikaRolfini@users.noreply.github.com>
2023-03-10 13:06:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import saluto_Candela_Gruppo_6
+import saluto
 
-saluto_Candela_Gruppo_6()
+saluto()
 
-import saluto_Cavallari_Gruppo_6
+import saluto_Cavallari
 
-saluto_Cavallari_Gruppo_6()
+saluto()
 
 def saluto_Rolfini_Gruppo_6():
     print("Saluti da Reika Rolfini del gruppo 6!")
