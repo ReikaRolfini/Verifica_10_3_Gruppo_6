09b34e8159a8e294dade3558cab7ac968e22eb2c
ReikaRolfini <reika.rolfini@polocodigoro.edu.it>
2023-03-10 12:34:24 +0100

    Questo è il main

---
+++ main.py
@@ -0,0 +1,11 @@
+import saluto_Candela_Gruppo_6
+
+saluto_Candela_Gruppo_6()
+
+import saluto_Cavallari_Gruppo_6
+
+saluto_Cavallari_Gruppo_6()
+
+def saluto_Rolfini_Gruppo_6():
+    print("Saluti da Reika Rolfini del gruppo 6!")
+saluto_Rolfini_Gruppo_6()
